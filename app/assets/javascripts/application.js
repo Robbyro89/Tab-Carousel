18283b92d25522beb5fd2db8b0c097616a603890
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-28 04:05:44 +0800

    final touch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,17 +30,7 @@ $(document).ready(function(){
 
 })
 
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
-
+/*
 $(document).ready(function() {
 
 	//rotation speed and timer
@@ -120,3 +110,16 @@ $(document).ready(function() {
 function rotate() {
 	$('#next').click();
 }
+*/
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
